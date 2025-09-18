3730a23a52e61e57a75064c62777e403e47dc034
Stefan <127923305+Heibero@users.noreply.github.com>
2025-09-18 16:07:59 +0200

    next 6 exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) return "ERROR"
+    let sumString = ""
+    for (let i = 0; i < num; i++) {
+        sumString = sumString + string
+    }
+    return sumString
 };
 
 // Do not edit below this line
